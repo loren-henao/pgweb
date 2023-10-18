60ddd4da233abfe7272f1700f97c878baeea9403
lorenas <lorensanchez149@gmail.com>
2023-10-17 21:44:54 -0500

    portafolio actualizado con redes sociales

--- script.js
+++ script.js
@@ -16,26 +16,53 @@ function seleccionar(){
     menuVisible = false;
 }
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
-    }
-}
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
+<section class="skills" id="skills">
+    <div class="contenido-seccion">
+        <h2>Habilidades</h2>
+        <div class="fila">
+            <div class="col">
+                <h3>Habilidades Técnicas</h3>
+                <div class="skill">
+                    <span>Javascript</span>
+                    <div class="barra-skill">
+                        <div class="progreso javascript"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <span>HTML & CSS</span>
+                    <div class="barra-skill">
+                        <div class="progreso htmlcss"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <span>Photoshop</span>
+                    <div class="barra-skill">
+                        <div class="progreso photoshop"></div>
+                    </div>
+                </div>
+                <div class "skill">
+                    <span>WordPress</span>
+                    <div class="barra-skill">
+                        <div class="progreso wordpress"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <span>Drupal</span>
+                    <div class="barra-skill">
+                        <div class="progreso drupal"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <h3>Habilidades Profesionales</h3>
+                <div class="skill">
+                    <span>Comunicación</span>
+                    <div class="barra-skill">
+                        <div class="progreso comunicacion"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <span>Trabajo en Equipo</span>
+                    <div class="barra-skill">
+                        <div class="progreso trabajo"></div>
+                    </div>
